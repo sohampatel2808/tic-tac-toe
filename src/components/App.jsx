a6cd023f80d50c07d55fed170b5327635de9dc99
Soham Patel <soham.patel2808@gmail.com>
2022-06-30 22:30:10 +0530

    lifting state up to app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,83 @@ import React from 'react';
 import './App.css';
 import Board from './Board';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Board />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      squares: Array(9).fill(null),
+      currPlayerX: true
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Board
+          squares={this.state.squares}
+          handleClick={this.handleClick}/>
+
+        <div className="game-status">
+          {this.getGameStatus()}
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  handleClick(id) {
+    if (this.state.squares[id] || this.calculateWinner(this.state.squares)) {
+      // value present in current square, do nothing
+      return;
+    }
+
+    const cloneSquares = this.state.squares.slice();
+
+    // set value for current square
+    cloneSquares[id] = this.state.currPlayerX ? 'X' : '0';
+
+    this.setState({
+      squares: cloneSquares,
+      currPlayerX: !this.state.currPlayerX
+    });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  getGameStatus() {
+    let status;
+    let winner = this.calculateWinner(this.state.squares)
+
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next Player: " + (this.state.currPlayerX ? 'X' : '0');
+    }
+
+    return status;
+  }
 }
 
 export default App;
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,30 +7,14 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      squares: Array(9).fill(null),
-      currPlayerX: true
-    }
+    this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
-    let playerInfo;
-    const winner = this.calculateWinner(this.state.squares);
-
-    if (winner) {
-      playerInfo = "Winner: " + winner;
-    } else {
-      playerInfo = "Next Player: " + (this.state.currPlayerX ? 'X' : '0');
-    }
-
     return (
       <React.Fragment>
         <div class="board-game">
           {this.renderSquares()}
-
-          <div className="player-info">
-            {playerInfo}
-          </div>
         </div>
       </React.Fragment>
     );
@@ -39,7 +23,7 @@ class Board extends React.Component {
   renderSquares() {
     let squares = [];
 
-    for (let i = 0; i < this.state.squares.length; i++) {
+    for (let i = 0; i < this.props.squares.length; i++) {
       squares.push(this.renderSquare(i));
     }
 
@@ -47,51 +31,15 @@ class Board extends React.Component {
   }
 
   renderSquare(id) {
-    const value = this.state.squares[id];
-
     return (
-      <Square value={value} onClick={this.handleClick.bind(this, id)} />
+      <Square 
+        value={this.props.squares[id]}
+        onClick={this.handleClick.bind(this, id)} />
     );
   }
 
-  calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-
-    return null;
-  }
-
   handleClick(id) {
-    if (this.state.squares[id] || this.calculateWinner(this.state.squares)) {
-      // value present in current square, do nothing
-      return;
-    }
-
-    const cloneSquares = this.state.squares.slice();
-
-    // set value for current square
-    cloneSquares[id] = this.state.currPlayerX ? 'X' : '0';
-
-    this.setState({
-      squares: cloneSquares,
-      currPlayerX: !this.state.currPlayerX
-    });
+    this.props.handleClick(id);
   }
 }
 
